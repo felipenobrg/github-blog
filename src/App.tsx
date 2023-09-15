8195b16db49de055618429f1f3a471b79603865b
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-15 13:45:17 -0300

    feat: creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./routes/routes";
 
-function App() {
+export function App() {
   return (
-    <>
-      
-    </>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
-
-export default App
