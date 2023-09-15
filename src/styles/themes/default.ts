5061cc56fcc319ecc6af37da9f88e33ebad200bb
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-15 13:49:53 -0300

    feat: changing default theme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,15 +1,14 @@
 export const defaultTheme = {
-    white: "#FFF",
-  
-    "gray-100": "#E1E1E6",
-    "gray-300": "#C4C4CC",
-    "gray-400": "#8D8D99",
-    "gray-500": "#7C7C8A",
-    "gray-600": "#323238",
-    "gray-700": "#29292E",
-    "gray-800": "#202024",
-    "gray-900": "#121214",
-  
-     "blue-300": "#5A7FFB",
-     "blue-400": "#1A2540",
-  };
+  white: "#fff",
+  blue: "#3294F8",
+  "base-title": "#E7EDF4",
+  "base-subtitle": "#C4D4E3",
+  "base-text": "#AFC2D4",
+  "base-span": "#7B96B2",
+  "base-label": "#3A536B",
+  "base-border": "#1C2F41",
+  "base-post": "#112131",
+  "base-profile": "#0B1B2B",
+  "base-background": "#071422",
+  "base-input": "#040F1A",
+} as const;
