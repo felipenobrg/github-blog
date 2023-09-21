76e94912be1783344810cbbda83012c7100cf78a
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-21 10:15:51 -0300

    feat: finishing the publicationCard

--- src/pages/home/components/publicationCard/index.tsx
+++ src/pages/home/components/publicationCard/index.tsx
@@ -1,19 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { PublicationCardContainer } from "./styles";
+import { CenteredPublicationCardContainer, PublicationCardContainer, PublicationCardContent } from "./styles";
 
-interface PublicationData {
-  items: {
-    id: number;
-    title: string;
-    body: string;
-  }[];
+interface PublicationItem {
+  id: number;
+  title: string;
+  body: string;
+  created_at: string; // Make sure this matches the data structure
 }
 
 export function PublicationCard() {
-  const [publicationData, setPublicationsData] = useState<PublicationData>({
-    items: [],
-  });
+  const [publicationData, setPublicationsData] = useState<PublicationItem[]>([]);
 
   useEffect(() => {
     async function getPublicationsData() {
@@ -21,27 +18,60 @@ export function PublicationCard() {
         const response = await axios.get(
           "https://api.github.com/search/issues?q=Boas%20práticas%20repo:rocketseat-education/reactjs-github-blog-challenge"
         );
-        setPublicationsData(response.data);
-        console.log(response.data);
+
+        setPublicationsData(response.data.items);
       } catch (error) {
         console.log(error);
       }
     }
+
     getPublicationsData();
   }, []);
 
-  const publicationsList = publicationData.items ? (
-    publicationData.items.map((item) => (
-      <PublicationCardContainer key={item.id}>
-        <div>
-          <h1>{item.title}</h1>
-          <p>{item.body}</p>
-        </div>
-      </PublicationCardContainer>
-    ))
-  ) : (
-    <p>Loading</p>
-  );
+  const calculateTimeAgo = (dateString: string) => {
+    const currentDate = new Date();
+    const createdAtDate = new Date(dateString);
+  
+    const monthsAgo =
+      (currentDate.getFullYear() - createdAtDate.getFullYear()) * 12 +
+      (currentDate.getMonth() - createdAtDate.getMonth());
+  
+    if (monthsAgo === 0) {
+      const daysAgo = Math.floor(
+        (currentDate.getTime() - createdAtDate.getTime()) /
+          (1000 * 60 * 60 * 24)
+      );
+      return `${daysAgo} dias`;
+    }
+  
+    return `${monthsAgo} meses`;
+  };
+  
+  const truncateText = (text: string, maxLength: number) => {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + "...";
+    }
+  };
 
-  return <>{publicationsList}</>;
+  return (
+    <CenteredPublicationCardContainer>
+    <PublicationCardContainer>
+      {publicationData.length > 0 ? (
+        publicationData.map((item) => (
+          <div key={item.id}>
+            <PublicationCardContent>
+              <div className="name-time">
+              <h1>{item.title}</h1>
+              <p>{calculateTimeAgo(item.created_at)}</p>
+              </div>
+              <p>{truncateText(item.body, 300)}</p>
+            </PublicationCardContent>
+          </div>
+        ))
+      ) : (
+        <p>Loading</p>
+      )}
+    </PublicationCardContainer>
+    </CenteredPublicationCardContainer>
+  );
 }
--- src/pages/home/components/publicationCard/styles.ts
+++ src/pages/home/components/publicationCard/styles.ts
@@ -1,5 +1,46 @@
 import styled from "styled-components";
 
-export const PublicationCardContainer = styled.div`
+export const CenteredPublicationCardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
-`
+export const PublicationCardContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  gap: 2rem;
+  margin-top: 2rem;
+`;
+
+export const PublicationCardContent = styled.div`
+  background: ${(props) => props.theme["base-post"]};
+  width: 25.9rem;
+  height: 17rem;
+  padding: 2rem;
+  border-radius: 8px;
+  cursor: pointer;
+  border: 2px solid transparent;
+  transition: border 0.3s ease;
+
+  .name-time {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+
+    h1 {
+      color: ${(props) => props.theme["base-title"]};
+      font-size: 20px;
+      margin-bottom: 1rem;
+      width: 68%;
+    }
+  }
+
+  &:hover {
+    border: 3px solid ${(props) => props.theme["base-border"]};
+  }
+
+  p {
+    color: ${(props) => props.theme["base-text"]};
+  }
+`;
